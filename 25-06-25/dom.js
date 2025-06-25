5366a8911f8ba146215e7ea0bd36277bf136c317
lokeshwarbanoth <lokeshwarbanoth@gmail.com>
2025-06-25 15:17:50 +0530

    DOM BASCICS

---
+++ 25-06-25/dom.js
@@ -0,0 +1,10 @@
+document.title="Dom in javascript";
+document.getElementById("header").innerHTML="Hello world";
+document.getElementById("header").style.color="white";
+document.getElementById("para").style.color="white";
+document.getElementById("para").style.fontSize="60px";
+document.getElementsByClassName("h")[0].style.color="blue";
+document.body.style.backgroundColor = "red";
+document.body.style.backgroundImage="url('idol.jpg')";
+document.body.style.backgroundPosition="top right";
+document.body.style.backgroundRepeat="no-repeat";
